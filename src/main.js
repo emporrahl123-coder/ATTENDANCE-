fafcd5fc952f523a8ba24c50d87c4630bd2dfa20
emporrahl123-coder <emporrahl123@gmail.com>
2025-11-16 09:21:08 +0300

    Initialize NestJS application with bootstrap function

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT || 3000);
+  console.log(`Server running on ${process.env.APP_URL || 'http://localhost:3000'}`);
+}
+bootstrap();
